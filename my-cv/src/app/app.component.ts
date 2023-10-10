ef93b896b42687e1d2634cb2ceaaa20af00fa423
Megan Moore <meganmoore.dev@gmail.com>
2023-10-10 10:35:02 -0400

    init

---
+++ my-cv/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-cv';
+  name = "Megan";
+  address = "7128 Fallsburg Rd";
+  phone = "4199614284";
+  email = "sdfsjh@gamil.com"
+}
---
+++ my-cv/src/app/work-experience/work-experience.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-experience',
+  templateUrl: './work-experience.component.html',
+  styleUrls: ['./work-experience.component.css']
+})
+export class WorkExperienceComponent {
+  company = "Heirlooms and Moore";
+  address = "Newark, OH";
+  phone = "419-961-4274";
+  
+}
